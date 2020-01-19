81123951bfafa4a513010505c0de4530295cf25e
muffajal53 <46901492+muffajal53@users.noreply.github.com>
2020-01-19 19:30:16 +0530

    Update sequence.py

--- sequence.py
+++ sequence.py
@@ -1,6 +1,7 @@
 # This program calculates the iterative sequence 
 # for the set of positive integers
 
+# create the iterative sequence for the given number
 def sequence(n):
 	print(n)
 	if (n==1):
@@ -12,8 +13,9 @@ def sequence(n):
 		n = int((3*n)+1)
 		return sequence(n)
 
+# Main method from which we call the sequence method
 if __name__ == '__main__':
-	# starting from here
+	# get the number from console for calculation
 	number = int(input("Please insert the number for iterative sequence :- "))
-	# call this function
+	# call sequence function
 	sequence(number)
